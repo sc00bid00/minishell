19c9d4e94cce87387471806b5af0a2741c257d2a
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-02-13 19:23:02 +0100

    Modify tokenizer test files 	modified:   inc/parse.h 	modified:   src_test/main.c 	deleted:    src_test/test_bit.c

--- inc/parse.h
+++ inc/parse.h
@@ -6,17 +6,18 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:53:45 by lsordo            #+#    #+#             */
-/*   Updated: 2023/02/13 16:50:35 by lsordo           ###   ########.fr       */
+/*   Updated: 2023/02/13 17:32:52 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <libft.h>
-typedef struct	s_token
+# include <unistd.h>
+# include <stdlib.h>
+# include <libft.h>
+
+typedef struct s_token
 {
 	t_list	*lst;
 	int		prev;
--- src_test/main.c
+++ src_test/main.c
@@ -6,49 +6,14 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:53:09 by lsordo            #+#    #+#             */
-/*   Updated: 2023/02/13 17:08:36 by lsordo           ###   ########.fr       */
+/*   Updated: 2023/02/13 19:00:50 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <parse.h>
 
-void	ft_chkquoting(t_token *tkn, char *str)
-{
-
-}
-
-void	ft_func(t_token *tkn, char *str)
-{
-	t_list	*tmp;
-
-	while (str && str[tkn->curr])
-	{
-		if (str[tkn->curr] == '\'')
-			tkn->chk = tkn->chk ^ 0b01;
-		if (str[tkn->curr] == '\"')
-			tkn->chk = tkn->chk ^ 0b10;
-		if (str[tkn->curr] == ' ' && tkn->chk == 0)
-		{
-			if (tkn->count == 0)
-				tkn->lst->content = ft_substr(str, tkn->prev, tkn->curr - tkn->prev);
-			else
-			{
-				tmp = ft_lstnew(ft_substr(str, tkn->prev, tkn->curr - tkn->prev));
-				ft_lstadd_back(&(tkn->lst), tmp);
-			}
-			tkn->count++;
-			tkn->prev = tkn->curr + 1;
-		}
-		tkn->curr++;
-	}
-	if (!str[tkn->curr] && tkn->prev != tkn->curr)
-		ft_lstadd_back(&(tkn->lst), ft_lstnew(ft_substr(str, tkn->prev, tkn->curr - tkn->prev)));
-}
-
-/* 	chk = 01	'	opened
-	chk = 10	"	opened */
-
-int		ft_token_init(t_token *tkn)
+/* return 0 initialized t_token */
+int	ft_token_init(t_token *tkn)
 {
 	tkn->lst = malloc(sizeof(t_list));
 	if (!tkn->lst)
@@ -56,8 +21,55 @@ int		ft_token_init(t_token *tkn)
 	tkn->count = 0;
 	tkn->curr = 0;
 	tkn->prev = 0;
-	tkn->chk = 0b00;
+	tkn->chk = 0b000;
 	return (0);
+}
+
+/* bit toggling to track open quotation marks */
+void	ft_bitgate(t_token *tkn, char *str)
+{
+	if (str[tkn->curr] == '\'')
+		tkn->chk = tkn->chk ^ 0b001;
+	if (str[tkn->curr] == '\"')
+		tkn->chk = tkn->chk ^ 0b010;
+	if (str[tkn->curr] != ' ')
+		tkn->chk = tkn->chk | 0b100;
+}
+
+/* return token splitting on space && multispace if no running quotes */
+void ft_splitspace(t_token *tkn, char *str)
+{
+	t_list	*tmp;
+
+	if (str[tkn->curr] == ' ' && !(tkn->chk & 0b011) && (tkn->chk & 0b100))
+	{
+		tkn->chk = tkn->chk ^ 0b100;
+		if (tkn->count == 0)
+			tkn->lst->content \
+			= ft_substr(str, tkn->prev, tkn->curr - tkn->prev);
+		else
+		{
+			tmp = \
+				ft_lstnew(ft_substr(str, tkn->prev, tkn->curr - tkn->prev));
+			ft_lstadd_back(&(tkn->lst), tmp);
+		}
+		tkn->count++;
+		tkn->prev = tkn->curr + 1;
+	}
+}
+
+/* return t_token with t_list of tokens */
+void	ft_token(t_token *tkn, char *str)
+{
+	while (str && str[tkn->curr])
+	{
+		ft_bitgate(tkn, str);
+		ft_splitspace(tkn, str);
+		tkn->curr++;
+	}
+	if (!str[tkn->curr] && tkn->prev != tkn->curr)
+		ft_lstadd_back(&(tkn->lst), \
+		ft_lstnew(ft_substr(str, tkn->prev, tkn->curr - tkn->prev)));
 }
 
 int	main(void)
@@ -70,8 +82,8 @@ int	main(void)
 		return (1);
 	if (ft_token_init(tkn))
 		return (1);
-	str = "\"< infile hallo\" cat -e | grep b | grep x | wc -l > outfile";
-	ft_func(tkn, str);
+	str = " <infile cat| wc -l  |ls -la   | echo \"so la la\" >>out";
+	ft_token(tkn, str);
 	while (tkn->lst)
 	{
 		ft_printf("%s\n", tkn->lst->content);
