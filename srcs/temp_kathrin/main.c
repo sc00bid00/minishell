0bacbcdd28eb3ac0869d87005b397450369471b7
Kathrin Czichowsky <kczichow@1-E-16.42heilbronn.de>
2023-02-23 16:41:19 +0100

    test new functions

--- srcs/temp_kathrin/main.c
+++ srcs/temp_kathrin/main.c
@@ -7,20 +7,29 @@
 
 
 
+// int main (int argc, char **argv, char **envp)
+// {
+// 	char **copy;
+// 	copy = copy_env_to_heap(envp);
+// 	temp_print_envs(copy);
+// 	signal(SIGQUIT, SIG_IGN);
+//     while (1)
+// 	{
+// 		setup_sigint();
+// 		get_input();
+// 		// restore_terminal();
+// 		// get_input();
+// 		//setup_sigexec(); put back default signals
+// 		// restore_terminal();
+// 	}
+// 	return (0);
+// }
+
 int main (int argc, char **argv, char **envp)
 {
-	char ** copy;
-	copy = copy_env_to_heap(envp);
-	temp_print_envs(copy);
-	signal(SIGQUIT, SIG_IGN);
-    while (1)
-	{
-		setup_sigint();
-		get_input();
-		// restore_terminal();
-		// get_input();
-		//setup_sigexec(); put back default signals
-		// restore_terminal();
-	}
-	return (0);
+	t_env 	*env;
+	env = NULL;
+	env = copy_envp_to_env(envp);
+	create_env_array(env);
+	builtin_pwd();
 }
