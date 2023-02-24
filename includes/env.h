2bb5406d8a0bc43d2421a5448c78d2a349825b71
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-02-24 15:38:08 +0100

    added function to header

--- includes/env.h
+++ includes/env.h
@@ -6,14 +6,12 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 09:52:24 by kczichow          #+#    #+#             */
-/*   Updated: 2023/02/23 14:29:15 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/02/24 13:08:18 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
-
-// # include "minishell.h"
 
 typedef struct s_env
 {
@@ -24,5 +22,8 @@ typedef struct s_env
 
 t_env	*copy_envp_to_env(char **envp);
 char	**create_env_array(t_env *env);
+void	print_env(t_env *env);
+t_env	*update_variable(t_env *env, char *var, char *new_val);
+char	*return_var_content(t_env *env, char *str);
 
 #endif
