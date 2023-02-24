fec4a2cdf3dc8382a3bd5cfbdc2dbe8d4fe8743a
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-02-24 15:42:39 +0100

    modified exit

--- srcs/builtin/builtin_pwd.c
+++ srcs/builtin/builtin_pwd.c
@@ -6,25 +6,28 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 15:55:31 by kczichow          #+#    #+#             */
-/*   Updated: 2023/02/23 16:19:04 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/02/24 10:14:31 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /* 	imitate behavior of pwd builtin */
-void	builtin_pwd(void)
+/*	getcwd returns pointer to path if sucessful, else it returns NULL */
+int_fast64_t	builtin_pwd(void)
 {
 	char	*pwd;
 
 	pwd = getcwd(NULL, 0);
-	if (pwd == NULL)
-	{
-		write (1, "\n", 1);
-	}
-	else
+	if (pwd)
 	{
 		ft_putendl_fd(pwd, 1);
 		free (pwd);
+		return (EXIT_SUCCESS);
+	}
+	else
+	{
+		write (1, "\n", 1);
+		return (EXIT_FAILURE);
 	}
 }
