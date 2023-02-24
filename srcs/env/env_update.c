028c17e65e8e76b5a5ec2e83eb87e9b7505bef33
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-02-24 15:45:45 +0100

    developed function to update pwd, gerenally modifiy nodes in the env list

--- srcs/env/env_update.c
+++ srcs/env/env_update.c
@@ -6,10 +6,52 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:01:37 by kczichow          #+#    #+#             */
-/*   Updated: 2023/02/23 14:04:23 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/02/24 15:09:38 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
 
-/* three vaiables need to be updated, pwd, old pwd, shell level */
+/* three variables need to be updated, pwd, old pwd, shell level */
+char	*return_var_content(t_env *env, char *str)
+{
+	t_env *temp;
+	
+	temp = env;
+	while (temp->next)
+	{
+		if (!ft_strncmp(temp->var_name, str, strlen(str)))
+			return (temp->var_content);
+		temp = temp->next;
+	}
+	return (NULL);
+}
+
+/* update existing node with new variable content */
+t_env	*update_variable(t_env *env, char *var, char *new_val)
+{
+	t_env *temp;
+	temp = env;
+	
+	while (temp->next)
+	{
+		if (!ft_strncmp(temp->var_name, var, ft_strlen(var)))
+		{
+			free(temp->var_content);
+			temp->var_content = ft_strdup(new_val);
+				return (temp);
+		}
+		temp = temp->next;
+	}
+	return (NULL);
+}
+
+t_env	*delete_variable(t_env *env, char *str)
+{
+	return (NULL);
+}
+
+t_env	*add_variable(t_env *env, char *str)
+{
+	return (NULL);
+}
