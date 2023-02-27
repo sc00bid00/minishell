4e0dbf439bb309eea0a136daa056eacb1c489dca
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-02-27 16:04:43 +0100

    new file for echo

---
+++ srcs/builtin/builtin_echo.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_echo.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 11:14:00 by kczichow          #+#    #+#             */
+/*   Updated: 2023/02/27 13:09:55 by kczichow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// int	builtin_echo(int argc, char **argv, t_env *env)
+// {
+// 	int i;
+// 	bool option;	
+
+// 	i = 1;
+// 	option = false;
+// 	if (argc == 1)
+// 		ft_putchar_fd("\n", 1);
+// 	else if (ft_strncmp(argv[3], "-n", 3))
+// 		option = true;
+// 	while (argv[i])
+// }
