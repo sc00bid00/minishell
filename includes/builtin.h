4165c2c9178734a2d55ea1e627a2b0b7937b46c3
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-02-27 16:13:57 +0100

    added functions to header

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,15 +6,17 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 16:16:01 by kczichow          #+#    #+#             */
-/*   Updated: 2023/02/24 13:10:54 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/02/27 11:23:14 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_H
 # define BUILTIN_H
 
-int_fast64_t	builtin_pwd(void);
-t_env	*builtin_cd(int argc, char **argv, t_env *env);
+int	builtin_pwd(void);
+int	builtin_cd(int argc, char **argv, t_env *env);
+int	builtin_env(int argc, char **argv, t_env *env);
+int	builtin_echo(int argc, char **argv, t_env *env);
 
 
 #endif
