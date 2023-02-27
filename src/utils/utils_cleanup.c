89a6e3ba7496579da8625c8934737c715b2d4469
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-02-27 17:01:42 +0100

    Modify
    	new file:   src/prexec/prexec.c
    	new file:   src/prexec/utils_prexec.c
    	new file:   src/utils/utils_cleanup.c
    	new file:   src/utils/utils_init.c
    	new file:   src/utils/utils_tmp.c

---
+++ src/utils/utils_cleanup.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_cleanup.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 15:08:58 by lsordo            #+#    #+#             */
+/*   Updated: 2023/02/27 13:26:48 by lsordo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <lexer.h>
+#include <parser.h>
+
+/* clean up t_list */
+void	ft_cleanlst(t_list *lst)
+{
+	t_list	*tmp;
+
+	while (lst)
+	{
+		tmp = lst->next;
+		free(lst->content);
+		free(lst);
+		lst = tmp;
+	}
+}
+
+/* clean up t-list ** */
+void	ft_cleanscmd(t_scmd *cmd)
+{
+	int		i;
+
+	i = 0;
+	while (cmd->arr[i])
+	{
+		ft_cleanlst(cmd->arr[i]);
+		i++;
+	}
+	free(cmd->arr);
+	free(cmd);
+}
+
+/* clear t_token allocation */
+void	ft_cleantkn(t_token *tkn)
+{
+	if (tkn->lst)
+		ft_cleanlst(tkn->lst);
+	if (tkn)
+		free(tkn);
+}
