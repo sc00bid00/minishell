c99042c8ffbd8e1faba103e105aa5fbf785f94bf
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-03-01 11:21:04 +0100

    updated file structure

--- inc/env.h
+++ inc/env.h
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 09:52:24 by kczichow          #+#    #+#             */
-/*   Updated: 2023/02/27 10:55:35 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/03/01 11:06:16 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,19 @@ typedef struct s_env
 	struct s_env	*next;
 }			t_env;
 
+/* env_build */
+char	*get_var_name(char *var);
+char	*get_var_content(char *var);
+t_env	*new_var(char *var);
 t_env	*copy_envp_to_env(char **envp);
+
+/* env_transform */
+void	print_env_array(char **env_array);
 char	**create_env_array(t_env *env);
-t_env	*update_variable(t_env *env, char *var, char *new_val);
-char	*return_var_content(t_env *env, char *str);
+
+/* env_update */
+t_env	*ret_var(t_env *env, char *str);
+t_env	*upd_var(t_env *env, char *var, char *new_val);
+int		*del_var(t_env *env, char *var);
 
 #endif
