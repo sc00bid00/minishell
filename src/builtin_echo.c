18266189a580a8531730299b4c927ce2e2f5d8d5
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-03-04 12:54:48 +0100

    Modify change arguments of  builtin_echo 	modified:   src/builtin_echo.c 	modified:   src/utils_executor.c

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,32 +6,31 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 11:14:00 by kczichow          #+#    #+#             */
-/*   Updated: 2023/03/04 10:06:15 by lsordo           ###   ########.fr       */
+/*   Updated: 2023/03/04 12:53:15 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
 /*	imitate echo with -n option */
-int	builtin_echo(int argc, char **argv, t_env *env)
+int	builtin_echo(t_cmd	*cmd)
 {
 	int		i;
 	bool	option;
 
-	(void)env;
-	i = 1;
+	i = 0;
 	option = false;
-	if (argc == 1)
+	if (cmd->argc == 1)
 		ft_putchar_fd('\n', 1);
-	else if (argc >= 3 && ft_strncmp(argv[2], "-n", 3))
+	else if (cmd->argc >= 3 && ft_strncmp(cmd->arr[1], "-n", 3))
 	{
 		option = true;
 		i++;
 	}
-	while (i < argc)
+	while (i < cmd->argc)
 	{
-		ft_putstr_fd(argv[i], 1);
-		if (argv[i + 1])
+		ft_putstr_fd(cmd->arr[i], 1);
+		if (cmd->arr[i + 1])
 			ft_putchar_fd(' ', 1);
 		i++;
 	}
@@ -39,12 +38,3 @@ int	builtin_echo(int argc, char **argv, t_env *env)
 		ft_putchar_fd('\n', 1);
 	return (0);
 }
-
-// 	i = 1;
-// 	option = false;
-// 	if (argc == 1)
-// 		ft_putchar_fd("\n", 1);
-// 	else if (ft_strncmp(argv[3], "-n", 3))
-// 		option = true;
-// 	while (argv[i])
-// }
