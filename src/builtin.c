c729622b125884f9327d813e5a857451a2a40048
Kathrin Czichowsky <kczichow@2-G-4.42heilbronn.de>
2023-03-17 10:11:07 +0100

    replaced functions with function pointer

--- src/builtin.c
+++ src/builtin.c
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 11:56:09 by kczichow          #+#    #+#             */
-/*   Updated: 2023/03/16 14:50:22 by kczichow         ###   ########.fr       */
+/*   Updated: 2023/03/17 10:05:07 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,24 @@
 
 int ft_builtin(t_scmd *scmd)
 {
-	int i = 0;
-	// printf("string is: %s\n",scmd->cmd[i]->arr[0]);
-	if (!ft_strncmp(scmd->cmd[i]->arr[0], "echo", 4))
-	{
-		builtin_echo(*scmd->cmd, scmd->env);
-		return (1);
-	}
-	else if (!ft_strncmp(scmd->cmd[i]->arr[0], "cd", 2))
-	{
-		builtin_cd(*scmd->cmd, scmd->env);
-		return (1);
-	}
-	else if (!ft_strncmp(scmd->cmd[i]->arr[0], "unset", 5))
-	{
-		builtin_unset(*scmd->cmd, scmd->env);
-		return (1);
-	}
-	else if (!ft_strncmp(scmd->cmd[i]->arr[0], "pwd", 3))
-	{
-		builtin_pwd(*scmd->cmd, scmd->env);
-		return (1);
-	}
-	else if (!ft_strncmp(scmd->cmd[i]->arr[0], "env", 3))
-	{
-		builtin_env(*scmd->cmd, scmd->env);
-		return (1);
-	}
-	else if (!ft_strncmp(scmd->cmd[i]->arr[0], "export", 6))
-	{
-		builtin_export(*scmd->cmd, scmd->env);
-		return (1);
-	}
+	int	(*fun)(t_cmd *, t_env *);
+	
+	if (!ft_strncmp(scmd->cmd[scmd->count]->arr[0], "exit", 4))
+		fun = &builtin_exit;
+	else if (!ft_strncmp(scmd->cmd[scmd->count]->arr[0], "echo", 4))
+		fun = &builtin_echo;
+	else if (!ft_strncmp(scmd->cmd[scmd->count]->arr[0], "cd", 2))
+		fun = &builtin_cd;
+	else if (!ft_strncmp(scmd->cmd[scmd->count]->arr[0], "unset", 5))
+		fun = &builtin_unset;
+	else if (!ft_strncmp(scmd->cmd[scmd->count]->arr[0], "pwd", 3))
+		fun = &builtin_pwd;
+	else if (!ft_strncmp(scmd->cmd[scmd->count]->arr[0], "env", 3))
+		fun = &builtin_env;
+	else if (!ft_strncmp(scmd->cmd[scmd->count]->arr[0], "export", 6))
+		fun = &builtin_export;
 	else
 		return (0);
+	fun(scmd->cmd[scmd->count], scmd->env);
+	return (1);
 }
