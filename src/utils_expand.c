19b13c006c0cb61035233a38743c6a21f1d0706a
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-04-03 14:38:06 +0200

    Modify correct ~ if unset HOME norm 	modified:   src/utils_expand.c

--- src/utils_expand.c
+++ src/utils_expand.c
@@ -6,37 +6,45 @@
 /*   By: lsordo <lsordo@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 10:14:18 by lsordo            #+#    #+#             */
-/*   Updated: 2023/03/30 17:53:42 by lsordo           ###   ########.fr       */
+/*   Updated: 2023/04/03 14:37:51 by lsordo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+void	ft_helpexptilde_1(t_token *tkn, t_list *lst)
+{
+	t_env	*env_var;
+	char	*tmp;
+
+	tmp = NULL;
+	if (((char *)lst->content)[1] == '/')
+	{
+		env_var = ret_var(&tkn->env, "HOME");
+		if (env_var && env_var->var_content)
+		{
+			tmp = ft_strjoin(env_var->var_content, &((char *)lst->content)[1]);
+			free(lst->content);
+			lst->content = tmp;
+		}
+	}
+}
+
 void	ft_exptilde(t_token *tkn)
 {
 	t_list	*lst;
 	t_env	*env_var;
-	char	*tmp;
 	int		i;
 
-	tmp = NULL;
 	lst = tkn->lst;
 	i = 0;
 	while (lst)
 	{
 		if (i < 3 && lst->content && ft_strchr((char *)lst->content, '~')
 			&& ft_strlen((char *)lst->content) > 1)
-		{
-			if (((char *)lst->content)[1] == '/')
-			{
-				env_var = ret_var(&tkn->env, "HOME");
-				if (env_var && env_var->var_content)
-					tmp = ft_strjoin(env_var->var_content, &((char *)lst->content)[1]);
-				free(lst->content);
-				lst->content = tmp;
-			}
-		}
-		else if (i < 3 && lst->content && !ft_strncmp((char *)lst->content, "~", 1))
+			ft_helpexptilde_1(tkn, lst);
+		else if (i < 3 && lst->content
+			&& !ft_strncmp((char *)lst->content, "~", 1))
 		{
 			env_var = ret_var(&tkn->env, "HOME");
 			if (env_var && env_var->var_content)
